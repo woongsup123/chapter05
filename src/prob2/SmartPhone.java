b9e8184d4fcf37dd2fac2671730f23afb410945b
bit-user <bit-user@DESKTOP-CNKQU76>
2017-08-03 20:29:01 +0900

    Complete Problem 2

--- src/prob2/SmartPhone.java
+++ src/prob2/SmartPhone.java
@@ -1,4 +1,29 @@
 package prob2;
 
 public class SmartPhone extends MusicPhone {
+	
+	@Override
+	public void execute( String function ) {
+		
+		if ( function == "음악") {
+			playMusic();
+		}
+		else if ( function == "통화") {
+			super.execute( function );
+		}
+		else if ( function == "앱") {
+			runApp();
+		}
+	}
+	
+	private void runApp() {
+		System.out.println("앱실행");
+	}
+
+	@Override
+	protected void playMusic() {
+		System.out.println("다운로드해서 음악재생");
+	}
+	
+	
 }
