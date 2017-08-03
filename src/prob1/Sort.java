d2f44cae9d1656685d0869aadde24a9eca7c94d4
bit-user <bit-user@DESKTOP-CNKQU76>
2017-08-03 16:47:22 +0900

    Implement bubble sort

--- src/prob1/Sort.java
+++ src/prob1/Sort.java
@@ -13,17 +13,32 @@ public class Sort {
 		for (int i = 0; i < count; i++) {
 			System.out.print( array[ i ] + " " );
 		}
-		
-		
-		//
-		// 정렬 알고리즘이 적용된 코드를 여기에 작성합니다.
-		// 
 	
+		bubbleSort(array);
 		
 		System.out.println( "\nAfter Sort." );
 		
 		for (int i = 0; i < count; i++) {
 			System.out.print(array[i] + " ");
 		}
+	}
+	
+	
+	public static void bubbleSort(int[] arr){
+		int maxPos = arr.length;
+		for (int i = 0; i < maxPos; i++) {
+			for (int j = 0; j < maxPos-1; j++) {
+				if (arr[j] < arr[j+1]) {
+					swap(j, j+1, arr);
+				}
+			}
+			maxPos--;
+		}
+	}
+	
+	public static void swap(int i, int j, int[] arr) {
+		int n = arr[i];
+		arr[i] = arr[j];
+		arr[j] = n;
 	}
 }
