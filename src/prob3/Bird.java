c1f6a271b13aeaf2526c7914ec6d93356b251d52
bit-user <bit-user@DESKTOP-CNKQU76>
2017-08-03 20:36:37 +0900

    Complete Problem 3

--- src/prob3/Bird.java
+++ src/prob3/Bird.java
@@ -1,4 +1,18 @@
 package prob3;
 
 public abstract class Bird {
+	
+	private String name;
+	
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	public String getName() {
+		return name;
+	}
+
+	public abstract void sing();
+	
+	public abstract void fly();
 }
